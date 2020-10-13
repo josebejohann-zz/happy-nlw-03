bc33e568f994e1a2561092ea3da9a31f938a00a6
josebejohann <josebernardojohann@gmail.com>
2020-10-13 17:59:02 -0300

    Create landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import Logo from './assets/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Happy</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={Logo} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo.</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Florianópolis</strong>
+          <span>Santa Catarina</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
+      </div>
     </div>
   );
 }
